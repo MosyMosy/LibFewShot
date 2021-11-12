85161946b3bb0556969a62daf0408021b789cc2e
Moslem Yazdanpanah <Moslem.Yazdanpanah@gmail.com>
2021-11-12 22:53:39 +0330

    new resume

--- run_trainer_resume.py
+++ run_trainer_resume.py
@@ -1,12 +1,11 @@
 # -*- coding: utf-8 -*-
+from core import Trainer
+from core import Test
+from core.config import Config
+import os
 import sys
 import argparse
 sys.dont_write_bytecode = True
-
-import os
-
-from core.config import Config
-from core import Trainer
 
 
 parser = argparse.ArgumentParser(description='LibFewShot Testing')
@@ -16,7 +15,26 @@ parser.add_argument("-resume_path", "--resume_path",
 
 if __name__ == "__main__":
     args = parser.parse_args()
-    PATH = args.resume_path
-    config = Config(os.path.join(PATH, "config.yaml"), is_resume=True).get_config_dict()
+    config = Config(os.path.join(args.resume_path, "config.yaml"),
+                    is_resume=True).get_config_dict()
     trainer = Trainer(config)
     trainer.train_loop()
+
+    # Testing
+    print('')
+    print("----------------------------------------------------------------------------")
+    print("--------------------------------   Testing   -------------------------------")
+    print("----------------------------------------------------------------------------")
+
+    VAR_DICT = {
+        "test_epoch": 5,
+        "n_gpu": 1,
+        "test_episode": 2000,
+        "episode_size": 1,
+        "test_way": 5,
+    }
+    args = parser.parse_args()
+    config = Config(os.path.join(args.resume_path, "config.yaml"),
+                    VAR_DICT).get_config_dict()
+    test = Test(config, args.resume_path)
+    test.test_loop()
