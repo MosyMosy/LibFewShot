dba1d3c5281ed7d442aa1679a345837d8f3de2a8
Moslem Yazdanpanah <Moslem.Yazdanpanah@gmail.com>
2021-11-12 23:41:46 +0330

    new resume

---
+++ jobs/resume_maml_mini_5_na.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+#SBATCH --mail-user=Moslem.Yazdanpanah@gmail.com
+#SBATCH --mail-type=BEGIN
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-type=REQUEUE
+#SBATCH --mail-type=ALL
+#SBATCH --job-name=resume_maml_mini_5_na
+#SBATCH --output=%x-%j.out
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:4
+#SBATCH --ntasks-per-node=32
+#SBATCH --mem=127000M
+#SBATCH --time=4-00:00
+#SBATCH --account=rrg-ebrahimi
+
+nvidia-smi
+
+module load python/3.7
+source ~/ENV/bin/activate
+
+echo "------------------------------------< Data preparation>----------------------------------"
+echo "Copying the source code"
+date +"%T"
+cd $SLURM_TMPDIR
+cp -r ~/scratch/LibFewShot .
+
+echo "Copying the datasets"
+date +"%T"
+cp -r ~/scratch/dataset/LibFewShot/* .
+
+echo "Extract to dataset folder"
+date +"%T"
+cd LibFewShot/dataset
+
+# tar -xf $SLURM_TMPDIR/CIFAR100.tar.gz
+# tar -xf $SLURM_TMPDIR/CUB_200_2011_FewShot.tar.gz
+# tar -xf $SLURM_TMPDIR/CUB_birds_2010.tar.gz
+# tar -xf $SLURM_TMPDIR/StanfordCar.tar.gz
+# tar -xf $SLURM_TMPDIR/StanfordDog.tar.gz
+
+tar -xf $SLURM_TMPDIR/miniImageNet--ravi.tar.gz
+cat $SLURM_TMPDIR/tieredImageNet.tar.gz* | tar -zxf -
+
+echo "----------------------------------< End of data preparation>--------------------------------"
+date +"%T"
+echo "--------------------------------------------------------------------------------------------"
+
+echo "---------------------------------------<Run the program>------------------------------------"
+date +"%T"
+
+cd ..
+python run_trainer_resume.py --resume_path ./results/MAML-miniImageNet--ravi-Conv64F_no_affine-5-5-freeitter-Nov-10-2021-22-52-12/
+
+wait
+
+cd $SLURM_TMPDIR
+cp -r $SLURM_TMPDIR/LibFewShot/results/MAML-miniImageNet--ravi-Conv64F_no_affine-5-5-freeitter-Nov-10-2021-22-52-12 ~/scratch/LibFewShot/results/
